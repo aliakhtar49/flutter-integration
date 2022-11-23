ac5b18520df696c25c9a87e5c19d1fbd2cd7297e
Ali Akhtar <ali.akhtar@hungerstation.com>
2022-11-23 14:08:43 +0500

    initial commit

---
+++ FlutterIntegration/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FlutterIntegration
+//
+//  Created by Hungerstation on 23/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func openFlutterScreen(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
