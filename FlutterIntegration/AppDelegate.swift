d43e9cfab410e850ab54e7732dc00c75ad7ccfb7
Ali Akhtar <ali.akhtar@hungerstation.com>
2022-12-15 23:47:27 +0500

    communication bwteen flutter and native code

--- FlutterIntegration/AppDelegate.swift
+++ FlutterIntegration/AppDelegate.swift
@@ -9,17 +9,42 @@ import UIKit
 import Flutter
 // The following library connects plugins with iOS platform code to this app.
 import FlutterPluginRegistrant
-
+struct User: Codable {
+    let name: String
+    let email: String
+}
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     lazy var flutterEngine = FlutterEngine(name: "my flutter engine")
-
+    var sendMessageChannel: FlutterMethodChannel!
+   
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         flutterEngine.run();
+       
+        sendMessageChannel = FlutterMethodChannel(
+            name: "my_flutter_app.sendMessage",
+            binaryMessenger: flutterEngine.binaryMessenger
+        )
+        sendMessageChannel.setMethodCallHandler({ [weak self]
+            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            let data = Data((call.arguments as! String).utf8)
+            let user = try! JSONDecoder().decode(User.self, from: data)
+//            let name = (call.arguments as! [String: String])["name"]!
+//            let email = (call.arguments as! [String: String])["email"]!
+            guard call.method == "sendMessage" else {
+                result(FlutterMethodNotImplemented)
+                return
+              }
+            self?.callSomeMethod(name: user.name, email: user.email, result: result)
+        })
         GeneratedPluginRegistrant.register(with: self.flutterEngine);
         // Override point for customization after application launch.
         return true
+    }
+    
+    private func callSomeMethod(name: String, email: String, result: FlutterResult) {
+        result("native ios code \(name)  \(email)")
     }
 
     // MARK: UISceneSession Lifecycle
--- FlutterIntegration/ViewController.swift
+++ FlutterIntegration/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 import Flutter
 import Kingfisher
+import FlutterPluginRegistrant
 
 class ViewController: UIViewController {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var messageToNativeCodeChannel: FlutterMethodChannel!
 
     @IBAction func openFlutterScreen(_ sender: Any) {
+        messageToNativeCodeChannel = FlutterMethodChannel(
+            name: "my_flutter_app.messageToNativeCodeChannel",
+            binaryMessenger: appDelegate.flutterEngine.binaryMessenger
+        )
+        let user = User(name: "rahim", email: "rahim@gmail.com")
+        let data = try! JSONEncoder().encode(user)
+        let userJsonStr = String(data: data, encoding: .utf8)
+        messageToNativeCodeChannel.invokeMethod("receieveMessage", arguments: userJsonStr)
+        
         let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
             let flutterViewController =
                 FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
